7b512cb543dd2f93eb3c5493b118b0d79309f03f
Shubham Goyal <786shubhamgoyal@gmail.com>
2026-06-02 08:04:35 +0000

    added deposit and withdraw instruction in api

---
+++ programs/flott/src/instructions/api/deposit.rs
@@ -0,0 +1,69 @@
+use anchor_lang::{
+  prelude::*,
+  system_program::{transfer, Transfer},
+};
+use crate::state::*;
+use crate::constants::*;
+use crate::error::ErrorCode;
+
+#[derive(Accounts)]
+pub struct DepositToVault<'info> {
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  
+  pub authority: SystemAccount<'info>,
+  
+  #[account(
+    mut,
+    seeds = [
+      "api".as_ref(),
+      "user".as_ref(),
+      "vault".as_ref(),
+      owner.key().as_ref(),
+      SERVER_AUTHORIZED_KEY.as_ref()
+    ],
+    bump = api_user.vault_bump
+  )]
+  pub vault: SystemAccount<'info>,
+  
+  #[account(
+    seeds = [
+      "api".as_ref(),
+      "user".as_ref(),
+      owner.key().as_ref()
+    ],
+    bump = api_user.bump,
+    has_one = owner @ ErrorCode::OwnerMismatch
+  )]
+  pub api_user: Account<'info, ApiUser>,
+  
+  pub system_program: Program<'info, System>,
+}
+
+impl<'info> DepositToVault<'info> {
+  pub fn handler(&mut self, amount: u64) -> Result<()> {
+    self.api_user.verify_authority(&self.authority.key())?;
+    
+    let balance_after = self.vault.lamports()
+      .checked_add(amount)
+      .ok_or(ErrorCode::Overflow)?;
+    
+    require!(
+      balance_after > API_USER_MIN_BALANCE,
+      ErrorCode::InsufficientDeposit
+    );
+    
+    transfer(
+      CpiContext::new(
+        self.system_program.key(),
+        Transfer {
+          from: self.owner.to_account_info(),
+          to:   self.vault.to_account_info(),
+        },
+      ),
+      amount,
+    )?;
+    
+    Ok(())
+  }
+}
---
+++ programs/flott/src/instructions/api/withdraw.rs
@@ -0,0 +1,82 @@
+use anchor_lang::{
+  prelude::*,
+  system_program::{transfer, Transfer},
+};
+use crate::state::*;
+use crate::constants::*;
+use crate::error::ErrorCode;
+
+#[derive(Accounts)]
+pub struct WithdrawFromVault<'info> {
+  #[account(mut)]
+  pub owner: Signer<'info>,
+  
+  pub authority: SystemAccount<'info>,
+  
+  #[account(
+    mut,
+    seeds = [
+      "api".as_ref(),
+      "user".as_ref(),
+      "vault".as_ref(),
+      owner.key().as_ref(),
+      SERVER_AUTHORIZED_KEY.as_ref()
+    ],
+    bump = api_user.vault_bump
+  )]
+  pub vault: SystemAccount<'info>,
+  
+  #[account(
+    seeds = [
+      "api".as_ref(),
+      "user".as_ref(),
+      owner.key().as_ref()
+    ],
+    bump = api_user.bump,
+    has_one = owner @ ErrorCode::OwnerMismatch
+  )]
+  pub api_user: Account<'info, ApiUser>,
+  
+  pub system_program: Program<'info, System>,
+}
+
+impl<'info> WithdrawFromVault<'info> {
+  pub fn handler(&mut self, amount: u64) -> Result<()> {
+    self.api_user.verify_authority(&self.authority.key())?;
+    
+    let balance_after = self.vault.lamports()
+      .checked_sub(amount)
+      .ok_or(ErrorCode::Underflow)?;
+    
+    require!(
+      balance_after > API_USER_MIN_BALANCE,
+      ErrorCode::InsufficientDeposit
+    );
+    
+    let owner_key     = self.owner.key();
+    let vault_bump    = [self.api_user.vault_bump];
+    
+    let vault_seeds: &[&[u8]] = &[
+      b"api",
+      b"user",
+      b"vault",
+      owner_key.as_ref(),
+      SERVER_AUTHORIZED_KEY.as_ref(),
+      &vault_bump,
+    ];
+    
+    transfer(
+      CpiContext::new_with_signer(
+        self.system_program.key(),
+        Transfer {
+          from:   self.vault.to_account_info(),
+          to: self.owner.to_account_info(),
+        },
+        &[vault_seeds],
+      ),
+      amount,
+    )?;
+    
+    Ok(())
+  }
+}
